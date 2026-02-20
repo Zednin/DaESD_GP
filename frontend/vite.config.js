db02fad884f1672bd9a026d5f139da906b9826d1
samx32 <sam.waxman100@gmail.com>
2026-02-20 17:20:21 +0000

    BRFN-58: Added auto refresh (HMR) with react and vite for development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react({ fastRefresh: false })],
+  plugins: [react()],
   server: {
     port: 5173,
     strictPort: true,
     cors: true,
+    watch: {
+      usePolling: true,
+    },
   },
 });
