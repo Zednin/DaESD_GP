d64bf7f3557739e9e899a30bf4e7e5bdef5d0f16
DYL2J <dylan10.jones@live.uwe.ac.uk>
2026-02-20 15:35:41 +0000

    DRF for addresses testing added

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from apps.catalog.views import ProductViewSet
+from apps.addresses.views import AddressViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
+router.register(r'addresses', AddressViewSet, basename='address')
 urlpatterns = [
     path("", include(router.urls)),
 ]
