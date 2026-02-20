72629d17cad20038ffa517ae155c87c05d055d68
Matt <mattnogodula@gmail.com>
2026-02-20 12:23:01 +0000

    BRFN-51 testing a comit

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -12,16 +12,18 @@ class Account(AbstractUser):
         ('producer', 'Producer'),
         ('admin', 'Admin'),
     ]
-    
+    #hi
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPE_CHOICES, default='customer')
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.username
     
 class Customer(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE, related_name='customer_profile')
+
 
     def __str__(self):
         return f"Customer: {self.account.first_name} {self.account.last_name}"
