bdec49542ea8af12e3b8f22f3770bd3d46e880bb
Harrison Mann <mannharrison3@icloud.com>
2026-02-26 13:13:28 +0000

    Two issues fixed: 1. Dockerfile was modified to force Debian repos to use HTTPS instead of HTTP, which is necessary for environments where HTTP/80 is blocked. 2. Cart views fixed

--- backend/apps/cart/views.py
+++ backend/apps/cart/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import CartItem
-from .serializers import CartItemSerializer
+from .models import CartItem, Cart
+from .serializers import CartItemSerializer, CartSerializer
 
 
 class CartItemViewSet(ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
+    
+class CartViewSet(ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    
