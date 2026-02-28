4d62ac1a36b5cdc569d7f02215fd6d02dea537fc
Matt <mattnogodula@gmail.com>
2026-02-28 18:55:05 +0000

    Added more tables:
    /Catalogue
    - Inventory Adjustment
    
    /Traceability
    - Allergens
    
    /Community
    - Reviews
    - Recipe
    - Farm Story
    
    /Sustainability
    - Distance Record
    - Surplus

--- backend/apps/community/admin.py
+++ backend/apps/community/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import FarmStory, Recipe, Review
+
+@admin.register(FarmStory)
+class FarmStoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in FarmStory._meta.fields]
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Recipe._meta.fields]
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Review._meta.fields]
+
--- backend/apps/traceability/admin.py
+++ backend/apps/traceability/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Allergen
+
+@admin.register(Allergen)
+class AllergenAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Allergen._meta.fields]
