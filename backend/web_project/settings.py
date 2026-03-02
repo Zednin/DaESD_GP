2ed442a32178311621bf35b3080ff573f94e47b9
samx32 <sam.waxman100@gmail.com>
2026-03-02 17:12:30 +0000

    BRFN-63: Back end changes for product filtering and search

--- backend/web_project/settings.py
+++ backend/web_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sites", 
     "rest_framework",
     "rest_framework.authtoken",
+    "django_filters",
     "dj_rest_auth",
     "allauth",
     "allauth.account",
