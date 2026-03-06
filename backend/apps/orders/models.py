9b88d404222a02bc7bdbc673827c177bb9b686a0
joshokan <thejoshokanlawon@gmail.com>
2026-03-06 11:54:52 +0000

    BRFN-66: Checkout working with stripe integration, orders populated in the database correctly.

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -25,6 +25,8 @@ class Order(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    stripe_session_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
 
     def __str__(self):
         return f"Order {self.id} - {self.account.username}"
