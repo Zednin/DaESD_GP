e00d9750511b5679fd4c5fea4c0719c86e2f4f36
Harrison Mann <mannharrison3@icloud.com>
2026-03-27 09:54:30 +0000

    Fixed google login

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -53,9 +53,14 @@ urlpatterns = [
     path("", include(router.urls)),
     # Registration endpoints
     path("auth/", include("dj_rest_auth.urls")),
+    
+    #Google Accounts Auth
+    path("accounts/", include("allauth.urls")),
+    
     # Custom registration endpoints
     path("auth/register/customer/", CustomerRegisterView.as_view(), name="customer-register"),
     path("auth/register/producer/", ProducerRegisterView.as_view(), name="producer-register"),
+    
     
     path("checkout/create-session/", CreateCheckoutSessionView.as_view()),
     path("stripe/webhook/", stripe_webhook),
