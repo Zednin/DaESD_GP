a0e6ee301a3af049a98fc4df2107c49259628098
Harrison Mann <mannharrison3@icloud.com>
2026-04-21 22:03:50 +0100

    More About section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,7 @@ import styles from "./About.module.css";
 import AboutHero from "../components/About/AboutHero";
 import AboutTimeline from "../components/About/AboutTimeline";
 import AboutValues from "../components/About/AboutValues";
+import AboutImpact from "../components/About/AboutImpact";
 
 // import AboutImpact from "../components/About/AboutImpact";
 // import AboutCTA from "../components/About/AboutCTA";
@@ -12,9 +13,7 @@ export default function About() {
       <AboutHero />
       <AboutTimeline />
       <AboutValues />
-      {/* Add these next */}
-      
-      {/* <AboutImpact /> */}
+      <AboutImpact />
       {/* <AboutCTA /> */}
     </main>
   );
