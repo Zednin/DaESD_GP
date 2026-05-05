0a5bf7c6fc0af15b926f7c3b0f1d869a8dbddfab
Harrison Mann <mannharrison3@icloud.com>
2026-05-05 14:24:45 +0100

    Pushed changes to MacroMain Includes test file for API endpoints - very basic but useful as identfied some isssues which have now been resoleved

--- backend/apps/catalog/serializers.py
+++ backend/apps/catalog/serializers.py
@@ -85,6 +85,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
         read_only_fields = [
             "id",
+            "producer",
             "created_at",
             "updated_at",
             "surplus_price",
