d7d5878d9acc71013e82848be0afdadf12bb0a26
DYL2J <dylan10.jones@live.uwe.ac.uk>
2026-05-05 17:02:39 +0100

    file to aid in merge

---
+++ backend/apps/community/migrations/0005_merge_farmstorylike_review_account.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("community", "0004_farmstorylike"),
+        ("community", "0004_remove_review_uniq_review_product_customer_and_more"),
+    ]
+
+    operations = []
